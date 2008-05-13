a3d60a9117911f426c83d784fa0140d9a4b3d8bf
Scott Taylor <smt@scott-taylors-macbook.local>
2008-05-12 23:12:03 -0400

    * Spec helpers (for database)

---
+++ spec/spec_helpers.rb
@@ -0,0 +1,18 @@
+module SpecHelperFunctions
+  def setup_database_connection
+    require 'rubygems'
+    require 'sqlite3'
+    require 'active_record'
+    require 'active_support'
+    
+    ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database  => ':memory:'
+    ActiveRecord::Migration.verbose = false
+
+    ActiveRecord::Schema.define do  
+    end
+  end  
+end
+
+
+
+
