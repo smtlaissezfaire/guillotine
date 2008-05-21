d41299e6b13870c2b44bf5536b78b5d11eb891c6
Scott Taylor <smt@scott-taylors-macbook.local>
2008-05-20 21:11:20 -0400

    * Moving exception specs into it's own file

--- spec/cache_model/cached_model_spec.rb
+++ spec/cache_model/cached_model_spec.rb
@@ -33,19 +33,5 @@ module CacheModel
         @finder.find(:first).should == @person
       end
     end
-    
-    describe "finding an element with :first and an :include" do
-      it "should pass the argumetns onto the regular find" do
-        @target.should_receive(:find).with(:first, :include => :foo)
-        @finder.find(:first, :include => :foo)
-      end
-    end
-   
-    describe "finding an element with :all and an :include" do
-      it "should pass the argumetns onto the regular find" do
-        @target.should_receive(:find).with(:all, :include => :foo)
-        @finder.find(:all, :include => :foo)
-      end
-    end
   end
 end
