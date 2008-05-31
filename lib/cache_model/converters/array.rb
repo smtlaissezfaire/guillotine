42db3c765e756142b3bc74a602cc191e4d228f14
Scott Taylor <scott@railsnewbie.com>
2008-05-31 05:27:52 -0400

    * Removing the ugly-ass implementation

--- lib/cache_model/converters/array.rb
+++ lib/cache_model/converters/array.rb
@@ -47,44 +47,50 @@ private
     
     def initialize(ar_array)
       @original_ar_array = ar_array
-      @array = @original_ar_array.dup
+      @working_array = @original_ar_array.dup
     end
     
-    def parse
-      @statements = []
-      while next_statement?
-        @statements << [@current_key, @current_value, not_condition?]
+    attr_reader :working_array
+    
+    def parse(array = self.working_array, results=[])
+      @working_array = array
+      if expression.empty?
+        results
+      else
+        parse_sql_statement
+        parse(construct_new_array,
+              results << [@current_key, @current_value, not_condition?])
       end
-      @statements
     end
     
   private
-    
-    attr_accessor :array
     
     def not_condition?
       @current_condition == "!="
     end
     
     def next_statement?
-      find_next_statement
-    rescue
-      false
+      more_to_process?
     end
     
-    def find_next_statement
+    def more_to_process?
+      expression.any?
+    end
+    
+    def parse_sql_statement
       expression =~ REGEXP_MATCHER
-      
       @matching_expression = $1
       @current_key = $2.strip
       @current_condition = $3.strip
-      @current_value = array[1]
-      
-      destruct_old_array
+      @current_value = first_value
     end
     
-    def destruct_old_array
-      self.array = [rest_of_expression.strip, *rest_of_values]
+    def first_value
+      working_array[1]
+    end
+    
+    def construct_new_array
+      [rest_of_expression, *rest_of_values]
     end
     
     def matching_expression
@@ -92,15 +98,16 @@ private
     end
     
     def rest_of_expression
-      expression.gsub(matching_expression, "")
+      regex = Regexp.new("(AND){0,1}\s*#{Regexp.escape(matching_expression)}", Regexp::IGNORECASE)
+      expression.gsub(regex, "").strip
     end
     
     def expression
-      array[0]
+      @working_array[0]
     end
     
     def rest_of_values
-      array[2..array.length-1]
+      @working_array[2..@working_array.length-1]
     end
   end
 end
