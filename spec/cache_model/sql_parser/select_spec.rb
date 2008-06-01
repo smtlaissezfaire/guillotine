295e56d051c0e40ad3d60bdc241d32d1aeeb46c7
Scott Taylor <scott@railsnewbie.com>
2008-06-01 18:29:58 -0400

    * Adding backtick string support in parser

--- spec/cache_model/sql_parser/select_spec.rb
+++ spec/cache_model/sql_parser/select_spec.rb
@@ -7,53 +7,74 @@ module CachedModel
       @parser = SQLSelectParser.new
     end
     
-    def parse(val)
-      @parser.parse(val).eval
+    def parse(string)
+      @parser.parse(string)
+    end
+    
+    def parse_and_eval(string, *eval_args)
+      parse(string).eval(*eval_args)
     end
     
     describe "number" do
       it "should parse the number 0" do
-        parse('0').should == 0
+        parse_and_eval('0').should == 0
       end
       
       it "should parse the number 1" do
-        parse('1').should == 1
+        parse_and_eval('1').should == 1
       end
       
       it "should parse multiple numbers" do
-        parse('123').should == 123
+        parse_and_eval('123').should == 123
       end
       
       it "should not parse the empty string as a number" do
         lambda { 
-          parse('')
+          parse_and_eval('')
         }.should raise_error
       end
     end
     
     describe 'string' do
       it "should parse a single char" do
-        parse('a').should == "a"
+        parse_and_eval('a').should == "a"
       end
       
       it "should parse multitple chars" do
-        parse("aaaa").should == "aaaa"
+        parse_and_eval("aaaa").should == "aaaa"
       end
       
       it "should parse different chars" do
-        parse("abcd").should == "abcd"
+        parse_and_eval("abcd").should == "abcd"
       end
       
       it "should parse an uppercase char" do
-        parse("A").should == "A"
+        parse_and_eval("A").should == "A"
       end
       
       it "should parse a combination of uppercase and lowercase chars" do
-        parse("ABcdEF").should == "ABcdEF"
+        parse_and_eval("ABcdEF").should == "ABcdEF"
       end
       
       it "should parse underscores" do
-        parse("_").should == "_"
+        parse_and_eval("_").should == "_"
+      end
+    end
+    
+    describe "backtick string" do
+      before(:each) do
+        @backtick_string = mock("BacktickString")
+        @backtick_class = mock('BacktickStringClass', :new => @backtick_string)
+      end
+      
+      it "should parse a simple string properly" do
+        @backtick_class.should_receive(:new).with("foo").and_return @backtick_string
+        parse_and_eval("`foo`", @backtick_class)
+      end
+      
+      it "should parse a different string properly" do
+        @backtick_class.should_receive(:new).with("foobar").and_return @backtick_string
+        parse_and_eval("`foobar`", @backtick_class).should == @backtick_string
       end
     end
     
