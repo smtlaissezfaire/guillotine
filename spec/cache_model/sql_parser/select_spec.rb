2a1e4831665244e79bb1482d388151a8d2fea1e2
Scott Taylor <scott@railsnewbie.com>
2008-06-01 18:08:06 -0400

    * Cleaning up specs

--- spec/cache_model/sql_parser/select_spec.rb
+++ spec/cache_model/sql_parser/select_spec.rb
@@ -8,52 +8,52 @@ module CachedModel
     end
     
     def parse(val)
-      @parser.parse(val)
+      @parser.parse(val).eval
     end
     
     describe "number" do
       it "should parse the number 0" do
-        parse('0').eval.should == 0
+        parse('0').should == 0
       end
       
       it "should parse the number 1" do
-        parse('1').eval.should == 1
+        parse('1').should == 1
       end
       
       it "should parse multiple numbers" do
-        parse('123').eval.should == 123
+        parse('123').should == 123
       end
       
       it "should not parse the empty string as a number" do
         lambda { 
-          parse('').eval
+          parse('')
         }.should raise_error
       end
     end
     
     describe 'string' do
       it "should parse a single char" do
-        parse('a').eval.should == "a"
+        parse('a').should == "a"
       end
       
       it "should parse multitple chars" do
-        parse("aaaa").eval.should == "aaaa"
+        parse("aaaa").should == "aaaa"
       end
       
       it "should parse different chars" do
-        parse("abcd").eval.should == "abcd"
+        parse("abcd").should == "abcd"
       end
       
       it "should parse an uppercase char" do
-        parse("A").eval.should == "A"
+        parse("A").should == "A"
       end
       
       it "should parse a combination of uppercase and lowercase chars" do
-        parse("ABcdEF").eval.should == "ABcdEF"
+        parse("ABcdEF").should == "ABcdEF"
       end
       
       it "should parse underscores" do
-        parse("_").eval.should == "_"
+        parse("_").should == "_"
       end
     end
   end
