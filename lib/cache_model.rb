4fae5e4b75f507ab50888bd9f06a1ee5125ddd80
Scott Taylor <scott@railsnewbie.com>
2008-06-05 00:08:35 -0400

    * Adding keywords

--- lib/cache_model.rb
+++ lib/cache_model.rb
@@ -6,6 +6,7 @@ require File.dirname(__FILE__) + "/cache_model/converters"
 require File.dirname(__FILE__) + "/cache_model/active_record"
 
 require "treetop"
+require File.dirname(__FILE__) + "/cache_model/sql_parser/keyword_uppercaser"
 require File.dirname(__FILE__) + "/cache_model/treetop/backtick_string"
 Treetop.load File.dirname(__FILE__) + "/cache_model/treetop/sql_select"
 
