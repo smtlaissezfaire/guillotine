087d3e47a9c5d1ee81793664a5a68b3ab98dced6
Scott Taylor <scott@railsnewbie.com>
2008-06-10 05:01:13 -0400

    * Adding support for equal, notequal, lessthan, and so on * Half support for IS (not) NULL...it doesn't work yet

--- lib/cache_model/expression.rb
+++ lib/cache_model/expression.rb
@@ -1,2 +1,24 @@
 require File.dirname(__FILE__) + "/expression/base"
 require File.dirname(__FILE__) + "/expression/expressions"
+
+module CachedModel
+  module Expression
+    SYNTAX_CLASSES = { 
+      "="  => Equal,
+      "!=" => NotEqual,
+      "<"  => LessThan,
+      "<=" => LessThanOrEqualTo,
+      ">"  => GreaterThan,
+      ">=" => GreaterThanOrEqualTo,
+      "IS NOT NULL" => IsNotNull
+    }
+    
+    def self.find_class_for(joiner)
+      if syntax_class = SYNTAX_CLASSES[joiner]
+        syntax_class
+      else
+        raise "Unknown joiner #{joiner}"
+      end
+    end
+  end
+end
--- spec/cache_model/sql_parser/select_spec.rb
+++ spec/cache_model/sql_parser/select_spec.rb
@@ -2,9 +2,9 @@ require File.dirname(__FILE__) + "/../../spec_helper"
 
 module CachedModel
   
-  describe SQLSelectParser do
+  describe SQLPrimitivesParser do
     before :each do
-      @parser = SQLSelectParser.new
+      @parser = SQLPrimitivesParser.new
     end
     
     def parse(string)
@@ -139,11 +139,75 @@ module CachedModel
       it "should parse 'False' as false"
     end
     
+  end
+  
+  describe SQLSelectParser do
+    before :each do
+      @parser = SQLSelectParser.new
+    end
+    
+    def parse(string)
+      @parser.parse(string)
+    end
+    
+    def parse_and_eval(string, *eval_args)
+      parse(string).eval(*eval_args)
+    end
+    
     describe "condition" do
       describe "with '='" do
         it "should parse foo='bar'" do
-          pending 'todo'
           parse_and_eval("foo='bar'").should eql(Expression::Equal.new(:foo, "bar"))
+        end
+        
+        it "should parse baz='bar'" do
+          parse_and_eval("baz='bar'").should eql(Expression::Equal.new(:baz, "bar"))
+        end
+        
+        it "should parse foo='baz'" do
+          parse_and_eval("foo='baz'").should eql(Expression::Equal.new(:foo, "baz"))
+        end
+        
+        it "should parse foo=7" do
+          parse_and_eval("foo=7").should eql(Expression::Equal.new(:foo, 7))
+        end
+        
+        it "should parse foo = 7" do
+          parse_and_eval("foo = 7").should eql(Expression::Equal.new(:foo, 7))
+        end
+        
+        it "should parse foo != 8"  do
+          parse_and_eval("foo != 8").should eql(Expression::NotEqual.new(:foo, 8))
+        end
+        
+        it "should parse foo != \"barbaz\"" do
+          parse_and_eval("foo != \"barbaz\"").should eql(Expression::NotEqual.new(:foo, "barbaz"))
+        end
+        
+        it "should parse foo > 'bar'" do
+          parse_and_eval("foo > 'bar'").should eql(Expression::GreaterThan.new(:foo, "bar"))
+        end
+        
+        it "should parse foo < 'bar'" do
+          parse_and_eval("foo < 'bar'").should eql(Expression::LessThan.new(:foo, "bar"))
+        end
+        
+        it "should parse foo >= 7" do
+          parse_and_eval("foo >= 7").should eql(Expression::GreaterThanOrEqualTo.new(:foo, 7))
+        end
+        
+        it "should parse foo <= 7" do
+          parse_and_eval("foo <= 7").should eql(Expression::LessThanOrEqualTo.new(:foo, 7))
+        end
+        
+        it "should parse foo IS NOT NULL" do
+          pending 'todo'
+          parse_and_eval("foo IS NOT NULL").should eql(Expression::IsNotNull.new(:foo))
+        end
+        
+        it "should parse foo IS NULL" do
+          pending 'todo'
+          parse_and_eval("foo IS NULL").should eql(Expression::IsNull.new(:foo))
         end
       end
     end
