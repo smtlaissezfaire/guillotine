1dcb86b87c1f2f14ceb9bc0190cd704161b4b462
Scott Taylor <scott@railsnewbie.com>
2008-06-13 03:27:29 -0400

    * Forgot to add this file in last commit

---
+++ lib/cache_model/expression/select.rb
@@ -0,0 +1,16 @@
+module CachedModel
+  module Expression
+    class Select
+      def initialize(*columns)
+        @columns = columns
+      end
+      
+      attr_reader :columns
+      
+      # TODO: This should be more intelligent
+      def ==(other)
+        other.columns == self.columns
+      end
+    end
+  end
+end
