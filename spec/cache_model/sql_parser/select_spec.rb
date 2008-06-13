b80c6864a2e78af46aadf09d230d873af39bc4e5
Scott Taylor <scott@railsnewbie.com>
2008-06-12 23:52:04 -0400

    * WHERE ... AND ... now works (with as many subexpressions and parenthesis   as you'd like)

--- spec/cache_model/sql_parser/select_spec.rb
+++ spec/cache_model/sql_parser/select_spec.rb
@@ -242,11 +242,19 @@ module CachedModel
     before :each do
       @parser = SQLSelectParser.new
       @equal_expression = Expression::Equal.new(:foo, 7)
+
+      @foo_7 = @equal_expression
+      @bar_8 = Expression::Equal.new(:bar, 8)
+      @baz_9 = Expression::Equal.new(:baz, 9)
       @bar_equals_eight_expr = Expression::Equal.new(:bar, 8)
     end
     
     it "should parse a simple where clause with one expression" do
       parse_and_eval("WHERE foo = 7").should eql(@equal_expression)
+    end
+    
+    it "should parse a simple where clause with spaces" do
+      parse_and_eval("WHERE            foo    =  7").should eql(@equal_expression)
     end
     
     it "should parse a simple clause with two AND expressions" do
@@ -259,11 +267,46 @@ module CachedModel
       parse_and_eval("WHERE foo = 7 AND bar = 8").should eql(@and_expression)
     end
     
-    it "should parse a clause with three AND expressions" do
-      pending 'todo'
-      @first_and_expr = ConjunctionConditionNode.new(@equal_expression, @equal_expression)
-      @second_and_expr = ConjunctionConditionNode.new(@first_and_expr, @bar_equals_eight_expr)
-      parse_and_eval("WHERE foo = 7 AND foo = 7 AND bar = 8").should eql(@second_and_expr)
+    it "should parse a simple clause with two AND expressions, with random spaces in between the 'AND' condition" do
+      @and_expression = ConjunctionConditionNode.new(@equal_expression, @bar_equals_eight_expr)
+      parse_and_eval("WHERE             foo = 7 AND            bar    =  8").should eql(@and_expression)
     end
+    
+    it "should parse a clause with three AND expressions" do
+      @first_and_expr = ConjunctionConditionNode.new(@bar_8, @baz_9)
+      @second_and_expr = ConjunctionConditionNode.new(@foo_7, @first_and_expr)
+      node = parse_and_eval("WHERE foo = 7 AND bar = 8 AND baz=9")
+      node.should eql(@second_and_expr)
+    end
+    
+    it "should parse a clause with one expression and parenthesis" do
+      pending 'should it?'
+      parse_and_eval("WHERE (foo = 7)").should eql(@equal_expression)     
+    end
+    
+    it "should parse a clause with two expressions and parenthesis around them" do
+      and_expression = ConjunctionConditionNode.new(@equal_expression, @bar_equals_eight_expr)
+      parse_and_eval("WHERE (foo = 7 AND bar = 8)").should eql(and_expression)
+    end
+    
+    it "should parse a clause with two expressions and parenthesis around them with a space" do
+      and_expression = ConjunctionConditionNode.new(@equal_expression, @bar_equals_eight_expr)
+      parse_and_eval("WHERE ( foo = 7 AND bar = 8 )").should eql(and_expression)
+    end
+
+    it "should parse a clause with two expressions and parenthesis around them with multiple space" do
+      and_expression = ConjunctionConditionNode.new(@equal_expression, @bar_equals_eight_expr)
+      parse_and_eval("WHERE (          foo    =    7   AND    bar  = 8              )").should eql(and_expression)
+    end
+
+    it "should NOT parse a clause with two expressions and no matching parenthesis" do
+      parse("WHERE ( foo = 7 AND bar = 8").should be_nil
+    end
+    
+    it "should parse a clause with two parenthesis around it" do
+      and_expression = ConjunctionConditionNode.new(@equal_expression, @bar_equals_eight_expr)
+      parse_and_eval("WHERE ((foo = 7 AND bar = 8))").should eql(and_expression)
+    end
+    
   end
 end
