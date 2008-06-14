9b42030e58468b162f1cea6e0956ee254674159a
Scott Taylor <scott@railsnewbie.com>
2008-06-14 14:58:25 -0400

    * Moving the ParserSpecHelper to spec_helper.rb

--- spec/cache_model/sql_parser/select_spec.rb
+++ spec/cache_model/sql_parser/select_spec.rb
@@ -2,16 +2,6 @@ require File.dirname(__FILE__) + "/../../spec_helper"
 
 module CachedModel
   
-  module ParserSpecHelper
-    def parse(string)
-      @parser.parse(string)
-    end
-    
-    def parse_and_eval(string, *eval_args)
-      parse(string).eval(*eval_args)
-    end
-  end
-  
   describe SQLPrimitivesParser do
     include ParserSpecHelper
     
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
 
 require File.dirname(__FILE__) + "/../lib/cache_model"
 
+module ParserSpecHelper
+  def parse(string)
+    @parser.parse(string)
+  end
+  
+  def parse_and_eval(string, *eval_args)
+    parse(string).eval(*eval_args)
+  end
+end
