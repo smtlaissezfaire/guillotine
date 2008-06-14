d48c6d663714f221877e21610881651460815ff2
Scott Taylor <scott@railsnewbie.com>
2008-06-13 23:56:05 -0400

    * Moving treetop require to treetop.rb

--- lib/cache_model.rb
+++ lib/cache_model.rb
@@ -1,9 +1,7 @@
 
 require File.dirname(__FILE__) + "/cache_model/condition_tree"
-
 require File.dirname(__FILE__) + "/cache_model/sql_parser/keyword_uppercaser"
 require File.dirname(__FILE__) + "/cache_model/expression"
-require "treetop"
 require File.dirname(__FILE__) + "/cache_model/treetop/backtick_string"
 require File.dirname(__FILE__) + "/cache_model/treetop"
 
--- lib/cache_model/treetop.rb
+++ lib/cache_model/treetop.rb
@@ -1,4 +1,5 @@
 
+require "treetop"
 Treetop.load File.dirname(__FILE__) + "/treetop/primitives"
 Treetop.load File.dirname(__FILE__) + "/treetop/sql_key_value_pair"
 Treetop.load File.dirname(__FILE__) + "/treetop/sql_where_condition"
