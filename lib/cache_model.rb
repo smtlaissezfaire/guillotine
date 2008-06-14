ccd758c01dbdb6d01442052c60c8d20256a960e9
Scott Taylor <scott@railsnewbie.com>
2008-06-14 00:31:19 -0400

    * Renaming old KeywordUppercaser to keywords (the file)

--- lib/cache_model.rb
+++ lib/cache_model.rb
@@ -4,7 +4,7 @@ require "set"
 
 require File.dirname(__FILE__) + "/cache_model/extensions"
 require File.dirname(__FILE__) + "/cache_model/condition_tree"
-require File.dirname(__FILE__) + "/cache_model/sql_parser/keyword_uppercaser"
+require File.dirname(__FILE__) + "/cache_model/keywords"
 require File.dirname(__FILE__) + "/cache_model/expression"
 require File.dirname(__FILE__) + "/cache_model/treetop/backtick_string"
 require File.dirname(__FILE__) + "/cache_model/treetop"
--- spec/cache_model/keywords_spec.rb
+++ spec/cache_model/keywords_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + "/../../spec_helper"
+require File.dirname(__FILE__) + "/../spec_helper"
 
 module CacheModel
   describe Keywords do
