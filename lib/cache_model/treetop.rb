186fc4d58f8f5953cf374324fd2bd99ad51b5e56
Scott Taylor <scott@railsnewbie.com>
2008-06-13 23:54:24 -0400

    * Moving treetop requires into a single file

---
+++ lib/cache_model/treetop.rb
@@ -0,0 +1,5 @@
+Treetop.load File.dirname(__FILE__) + "/treetop/primitives"
+Treetop.load File.dirname(__FILE__) + "/treetop/sql_key_value_pair"
+Treetop.load File.dirname(__FILE__) + "/treetop/sql_where_condition"
+Treetop.load File.dirname(__FILE__) + "/treetop/sql_select"
+
