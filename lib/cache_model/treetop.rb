6792ad5de1637470b953fc9292dfbaa37ed3400e
Scott Taylor <scott@railsnewbie.com>
2008-06-14 14:54:26 -0400

    * Breaking off the limit clause into it's own parser

--- lib/cache_model/treetop.rb
+++ lib/cache_model/treetop.rb
@@ -3,5 +3,6 @@ require "treetop"
 Treetop.load File.dirname(__FILE__) + "/treetop/primitives"
 Treetop.load File.dirname(__FILE__) + "/treetop/sql_key_value_pair"
 Treetop.load File.dirname(__FILE__) + "/treetop/sql_where_condition"
+Treetop.load File.dirname(__FILE__) + "/treetop/sql_limit"
 Treetop.load File.dirname(__FILE__) + "/treetop/sql_select"
 
--- spec/cache_model/sql_parser/select_spec.rb
+++ spec/cache_model/sql_parser/select_spec.rb
@@ -366,6 +366,44 @@ module CachedModel
     end
   end
   
+  describe SQLLimitParser do
+    include ParserSpecHelper
+    
+    before :each do
+      @parser = SQLLimitParser.new
+    end
+    
+    describe "LIMIT" do
+      it "should not parse LIMIT" do
+        parse("LIMIT").should be_nil
+      end
+      
+      it "should parse LIMIT 10" do
+        parse_and_eval("LIMIT 10").should == Expression::Limit.new(10)
+      end
+      
+      it "should parse LIMIT 20" do
+        parse_and_eval("LIMIT 20").should == Expression::Limit.new(20)
+      end
+      
+      it "should parse LIMIT  30 (with spaces)" do
+        parse_and_eval("LIMIT  30").should == Expression::Limit.new(30)
+      end
+      
+      it "should not parse LIMIT30" do
+        parse("LIMIT30").should be_nil
+      end
+      
+      it "should parse LIMIT 0" do
+        parse_and_eval("LIMIT 0").should == Expression::Limit.new(0)
+      end
+      
+      it "should not parse LIMIT -1" do
+        parse("LIMIT -1").should be_nil
+      end
+    end
+  end
+  
   describe SQLSelectParser do
     include ParserSpecHelper
     
@@ -483,36 +521,6 @@ module CachedModel
       
       it "should not parse ORDER BYcolumn_name" do
         parse("ORDER BYcolumn_name").should be_nil
-      end
-    end
-    
-    describe "LIMIT" do
-      it "should not parse LIMIT" do
-        parse("LIMIT").should be_nil
-      end
-      
-      it "should parse LIMIT 10" do
-        parse_and_eval("LIMIT 10").should == Expression::Limit.new(10)
-      end
-      
-      it "should parse LIMIT 20" do
-        parse_and_eval("LIMIT 20").should == Expression::Limit.new(20)
-      end
-      
-      it "should parse LIMIT  30 (with spaces)" do
-        parse_and_eval("LIMIT  30").should == Expression::Limit.new(30)
-      end
-      
-      it "should not parse LIMIT30" do
-        parse("LIMIT30").should be_nil
-      end
-      
-      it "should parse LIMIT 0" do
-        parse_and_eval("LIMIT 0").should == Expression::Limit.new(0)
-      end
-      
-      it "should not parse LIMIT -1" do
-        parse("LIMIT -1").should be_nil
       end
     end
   end
