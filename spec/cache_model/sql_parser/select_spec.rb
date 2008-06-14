ea832c0f20fc5c211e4f03ea847851aef8a99c60
Scott Taylor <scott@railsnewbie.com>
2008-06-14 14:55:01 -0400

    * Removing unnecessary scoping in specs

--- spec/cache_model/sql_parser/select_spec.rb
+++ spec/cache_model/sql_parser/select_spec.rb
@@ -373,34 +373,32 @@ module CachedModel
       @parser = SQLLimitParser.new
     end
     
-    describe "LIMIT" do
-      it "should not parse LIMIT" do
-        parse("LIMIT").should be_nil
-      end
-      
-      it "should parse LIMIT 10" do
-        parse_and_eval("LIMIT 10").should == Expression::Limit.new(10)
-      end
-      
-      it "should parse LIMIT 20" do
-        parse_and_eval("LIMIT 20").should == Expression::Limit.new(20)
-      end
-      
-      it "should parse LIMIT  30 (with spaces)" do
-        parse_and_eval("LIMIT  30").should == Expression::Limit.new(30)
-      end
-      
-      it "should not parse LIMIT30" do
-        parse("LIMIT30").should be_nil
-      end
-      
-      it "should parse LIMIT 0" do
-        parse_and_eval("LIMIT 0").should == Expression::Limit.new(0)
-      end
-      
-      it "should not parse LIMIT -1" do
-        parse("LIMIT -1").should be_nil
-      end
+    it "should not parse LIMIT" do
+      parse("LIMIT").should be_nil
+    end
+    
+    it "should parse LIMIT 10" do
+      parse_and_eval("LIMIT 10").should == Expression::Limit.new(10)
+    end
+    
+    it "should parse LIMIT 20" do
+      parse_and_eval("LIMIT 20").should == Expression::Limit.new(20)
+    end
+    
+    it "should parse LIMIT  30 (with spaces)" do
+      parse_and_eval("LIMIT  30").should == Expression::Limit.new(30)
+    end
+    
+    it "should not parse LIMIT30" do
+      parse("LIMIT30").should be_nil
+    end
+    
+    it "should parse LIMIT 0" do
+      parse_and_eval("LIMIT 0").should == Expression::Limit.new(0)
+    end
+    
+    it "should not parse LIMIT -1" do
+      parse("LIMIT -1").should be_nil
     end
   end
   
