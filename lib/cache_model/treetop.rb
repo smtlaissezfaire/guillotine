90357d099a2a5824c7add0fec0a2813fa808fad1
Scott Taylor <scott@railsnewbie.com>
2008-06-14 15:12:08 -0400

    * Moving SQLFromClause into it's own file and parser

--- lib/cache_model/treetop.rb
+++ lib/cache_model/treetop.rb
@@ -5,5 +5,6 @@ Treetop.load File.dirname(__FILE__) + "/treetop/sql_key_value_pair"
 Treetop.load File.dirname(__FILE__) + "/treetop/sql_where_condition"
 Treetop.load File.dirname(__FILE__) + "/treetop/sql_limit"
 Treetop.load File.dirname(__FILE__) + "/treetop/sql_order_by_clause"
+Treetop.load File.dirname(__FILE__) + "/treetop/sql_from_clause"
 Treetop.load File.dirname(__FILE__) + "/treetop/sql_select"
 
