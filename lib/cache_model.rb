9f30f05cdc8a5b6b26c654bf8e7ff557dd489fab
Scott Taylor <scott@railsnewbie.com>
2008-06-13 23:45:20 -0400

    * Removing unnecessary (or at least untested) calls   to ActiveRecord

--- lib/cache_model.rb
+++ lib/cache_model.rb
@@ -1,6 +1,4 @@
 
-require "rubygems"
-require "activerecord"
 require File.dirname(__FILE__) + "/cache_model/cached_model"
 require File.dirname(__FILE__) + "/cache_model/active_record"
 require File.dirname(__FILE__) + "/cache_model/condition_tree"
