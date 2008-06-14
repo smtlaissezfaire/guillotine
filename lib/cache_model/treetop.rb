96f037e4d8ba5300525055121931ffb7f672125c
Scott Taylor <scott@railsnewbie.com>
2008-06-14 15:15:38 -0400

    * Moving row support into it's own treetop file

--- lib/cache_model/treetop.rb
+++ lib/cache_model/treetop.rb
@@ -1,6 +1,7 @@
 
 require "treetop"
 Treetop.load File.dirname(__FILE__) + "/treetop/primitives"
+Treetop.load File.dirname(__FILE__) + "/treetop/sql_row_support"
 Treetop.load File.dirname(__FILE__) + "/treetop/sql_key_value_pair"
 Treetop.load File.dirname(__FILE__) + "/treetop/sql_where_condition"
 Treetop.load File.dirname(__FILE__) + "/treetop/sql_limit"
