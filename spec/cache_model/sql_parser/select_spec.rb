52d7d76f1bfbefef58c6ebda28d1d77b06ffd28f
Scott Taylor <scott@railsnewbie.com>
2008-06-14 15:17:25 -0400

    * Moving the SQL Limit parser spec into it's own file

--- spec/cache_model/sql_parser/select_spec.rb
+++ spec/cache_model/sql_parser/select_spec.rb
@@ -1,7 +1,6 @@
 require File.dirname(__FILE__) + "/../../spec_helper"
 
 module CachedModel
-    
   describe SQLWhereConditionParser do
     include ParserSpecHelper
     
@@ -129,42 +128,6 @@ module CachedModel
         or_expression = DisjunctionConditionNode.new(@equal_expression, @bar_equals_eight_expr)
         parse_and_eval("WHERE ((foo = 7 OR bar = 8))").should eql(or_expression)
       end
-    end
-  end
-  
-  describe SQLLimitParser do
-    include ParserSpecHelper
-    
-    before :each do
-      @parser = SQLLimitParser.new
-    end
-    
-    it "should not parse LIMIT" do
-      parse("LIMIT").should be_nil
-    end
-    
-    it "should parse LIMIT 10" do
-      parse_and_eval("LIMIT 10").should == Expression::Limit.new(10)
-    end
-    
-    it "should parse LIMIT 20" do
-      parse_and_eval("LIMIT 20").should == Expression::Limit.new(20)
-    end
-    
-    it "should parse LIMIT  30 (with spaces)" do
-      parse_and_eval("LIMIT  30").should == Expression::Limit.new(30)
-    end
-    
-    it "should not parse LIMIT30" do
-      parse("LIMIT30").should be_nil
-    end
-    
-    it "should parse LIMIT 0" do
-      parse_and_eval("LIMIT 0").should == Expression::Limit.new(0)
-    end
-    
-    it "should not parse LIMIT -1" do
-      parse("LIMIT -1").should be_nil
     end
   end
   
