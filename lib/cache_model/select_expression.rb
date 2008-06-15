f193a2432ececbd21fd8f9be7857a009d3315734
Scott Taylor <scott@railsnewbie.com>
2008-06-15 03:11:56 -0400

    * Adding basics of the select

--- lib/cache_model/select_expression.rb
+++ lib/cache_model/select_expression.rb
@@ -4,17 +4,22 @@ module CachedModel
       @initial_hash = hash
       @select = hash[:select]
       @from = hash[:from]
-      @where = hash[:where] || nil
+      @where = hash[:where]
+      @limit = hash[:limit]
+      @order_by = hash[:order_by]
     end
 
     attr_reader :select
     attr_reader :from
     attr_reader :where
+    attr_reader :limit
+    attr_reader :order_by    
 
     def ==(other)
       other.select == self.select
       other.from   == self.from
       other.where  == self.where
+      other.limit  == self.limit
     end
 
   protected
--- spec/cache_model/treetop/sql_select_spec.rb
+++ spec/cache_model/treetop/sql_select_spec.rb
@@ -7,9 +7,12 @@ module CachedModel
     before :each do
       @parser = SQLSelectParser.new
     end
-
+    
     it "should parse 'SELECT * from events'" do
-      pending 'todo'
+      parse("SELECT * FROM events").should_not be_nil
+    end
+
+    it "should parse and evaluate 'SELECT * from events'" do
       expression = SelectExpression.new(:select => Expression::Select.new("*"), :from => Expression::From.new("events"))
       parse_and_eval("SELECT * FROM events").should == expression
     end
@@ -18,12 +21,56 @@ module CachedModel
       parse("SELECT * FROM events WHERE user = 'foo'").should_not be_nil
     end
 
-    it "should parse 'SELECT * from events where user = 'foo''" do
+    it "should parse and evaluate 'SELECT * from events where user = 'foo''" do
       select = Expression::Select.new("*")
       from   = Expression::From.new("events")
       where  = Expression::Equal.new(:user, "foo")
       expression = SelectExpression.new(:select => select, :from => from, :where => where)
       parse_and_eval("SELECT * FROM events WHERE user = 'foo'").should == expression
+    end
+    
+    it "should parse and evaluate SELECT   *    FROM     events     where user = 'foo'" do
+      select = Expression::Select.new("*")
+      from   = Expression::From.new("events")
+      where  = Expression::Equal.new(:user, "foo")
+      expression = SelectExpression.new(:select => select, :from => from, :where => where)
+      parse_and_eval("SELECT * FROM events WHERE user = 'foo'").should == expression
+    end
+    
+    it "should parse_and_eval SELECT * FROM events LIMIT 10" do
+      select = Expression::Select.new("*")
+      from   = Expression::From.new("events")
+      limit  = Expression::Limit.new(10)
+      expression = SelectExpression.new(:select => select, :from => from, :limit => limit)
+
+      parse_and_eval("SELECT * FROM events LIMIT 10")
+    end
+    
+    it "should parse_and_eval SELECT * FROM events        LIMIT 10" do
+      select = Expression::Select.new("*")
+      from   = Expression::From.new("events")
+      limit  = Expression::Limit.new(10)
+      expression = SelectExpression.new(:select => select, :from => from, :limit => limit)
+
+      parse_and_eval("SELECT * FROM events           LIMIT 10")
+    end
+
+    it "should parse_and_eval SELECT * FROM events ORDER BY foo" do
+      select = Expression::Select.new("*")
+      from   = Expression::From.new("events")
+      order_by  = Expression::OrderBy.new("foo")
+      expression = SelectExpression.new(:select => select, :from => from, :order_by => order_by)
+
+      parse_and_eval("SELECT * FROM events ORDER BY foo").should == expression
+    end
+
+    it "should parse_and_eval SELECT * FROM events              ORDER BY foo" do
+      select = Expression::Select.new("*")
+      from   = Expression::From.new("events")
+      order_by  = Expression::OrderBy.new("foo")
+      expression = SelectExpression.new(:select => select, :from => from, :order_by => order_by)
+
+      parse_and_eval("SELECT * FROM events ORDER BY foo").should == expression
     end
   end
 end
