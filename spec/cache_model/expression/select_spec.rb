f46428dbafdd6b1fdd53b0b885352d348a1c29d2
Scott Taylor <scott@railsnewbie.com>
2008-06-15 23:15:56 -0400

    * Adding query string and inspect to SelectExpression

---
+++ spec/cache_model/expression/select_spec.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + "/../../spec_helper"
+
+module CachedModel
+  describe SelectExpression do
+    it "should have the query string" do
+      SelectExpression.new(:string => "foo bar").query_string.should == "foo bar"
+    end
+    
+    it "should have the proper query string" do
+      SelectExpression.new(:string => "SELECT * FROM events").query_string.should == "SELECT * FROM events"
+    end
+    
+    it "should have a pretty inspect" do
+      query = "SELECT * FROM events"
+      expr = SelectExpression.new(:string => query)
+      expr.inspect.should == "CachedModel::SelectExpression: SELECT * FROM events"
+    end
+  end
+end
