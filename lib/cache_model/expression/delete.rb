662834637fb697b7e4f24068cf51c938749d0dce
Scott Taylor <scott@railsnewbie.com>
2008-06-18 23:05:47 -0400

    * Adding DeleteStatement implementation

--- lib/cache_model/expression/delete.rb
+++ lib/cache_model/expression/delete.rb
@@ -2,10 +2,22 @@ module CachedModel
   module Expression
     class DeleteStatement
       def initialize(table_name, where_clause=nil, order_by=nil, limit=nil)
+        @table_name = table_name.to_sym
+        @where = where_clause
+        @order_by = order_by
+        @limit = limit
       end
       
+      attr_reader :table_name
+      attr_reader :where
+      attr_reader :order_by
+      attr_reader :limit
+      
       def ==(other)
-        false
+        table_name.equal?(other.table_name) &&
+        where == other.where &&
+        order_by == other.order_by &&
+        limit == other.limit
       end
     end
   end
---
+++ spec/cache_model/expression/delete_spec.rb
@@ -0,0 +1,79 @@
+require File.dirname(__FILE__) + "/../../spec_helper"
+
+module CachedModel
+  module Expression
+    describe DeleteStatement do
+      before :each do
+        @where = mock('where clause')
+        @order_by = mock("order by clause")
+        @limit = mock("limit clause")
+        
+        @delete = DeleteStatement.new(:foo_bar, @where, @order_by, @limit)
+      end
+      
+      it "should have the table name" do
+        statement = DeleteStatement.new(:foo_bar)
+        statement.table_name.should equal(:foo_bar)
+      end
+      
+      it "should have the correct table name" do
+        statement = DeleteStatement.new(:foo_bar_baz)
+        statement.table_name.should equal(:foo_bar_baz)
+      end
+      
+      it "should have the table name as a symbol, even if given a string" do
+        statement = DeleteStatement.new("a_string")
+        statement.table_name.should equal(:a_string)
+      end
+      
+      it "should have a where clause as the second param" do
+        @delete.where.should == @where
+      end
+      
+      it "should have the order by clause as the third param" do
+        @delete.order_by.should == @order_by
+      end
+      
+      it "should have the limit clause as the forth param" do
+        @delete.limit.should == @limit
+      end
+      
+      describe "equality (with ==)" do
+        it "should be true with the same table names" do
+          one = DeleteStatement.new(:foo)
+          two = DeleteStatement.new("foo")
+          one.should == two
+          two.should == one
+        end
+        
+        it "should be false with different table names" do
+          one = DeleteStatement.new(:foo)
+          two = DeleteStatement.new("foobar")
+          one.should_not == two
+          two.should_not == one
+        end
+        
+        it "should be false if one has a where, while the other doesn't" do
+          one = DeleteStatement.new(:foo, @where)
+          two = DeleteStatement.new(:foo)
+          one.should_not == two
+          two.should_not == one
+        end
+        
+        it "should be false if one has an order by clause, when the other doesn't" do
+          one = DeleteStatement.new(:foo, nil, @order_by)
+          two = DeleteStatement.new(:foo)
+          one.should_not == two
+          two.should_not == one
+        end
+        
+        it "should be false if one has a limit clause, when the other doesn't" do
+          one = DeleteStatement.new(:foo, nil, nil, @limit)
+          two = DeleteStatement.new(:foo)
+          one.should_not == two
+          two.should_not == one
+        end
+      end
+    end
+  end
+end
