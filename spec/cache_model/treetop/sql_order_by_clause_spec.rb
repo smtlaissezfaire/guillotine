b3834686675da49929765946bf10936db9c6de56
Scott Taylor <scott@railsnewbie.com>
2008-06-19 01:49:31 -0400

    * Finishing off ASC, DESC support in Order by clause

--- spec/cache_model/treetop/sql_order_by_clause_spec.rb
+++ spec/cache_model/treetop/sql_order_by_clause_spec.rb
@@ -42,12 +42,20 @@ module CachedModel
       parse("ORDER BY col_one,   col_two").should_not be_nil
     end
     
-    it "should parse ORDER BY column_name ASC" do
-      parse("ORDER BY column_name ASC").should_not be_nil
+    it "should parse and eval ORDER BY column_name ASC" do
+      pair1 = Expression::OrderByPair.new(:column_name)
+      result = Expression::OrderBy.new(pair1)
+      parse_and_eval("ORDER BY column_name ASC").should == result
     end
     
     it "should parse ORDER BY column_name DESC" do
       parse("ORDER BY column_name DESC").should_not be_nil
+    end
+    
+    it "should parse and eval ORDER BY column_name DESC" do
+      pair1 = Expression::OrderByPair.new(:column_name, Expression::OrderBy::DESC)
+      result = Expression::OrderBy.new(pair1)
+      parse_and_eval("ORDER BY column_name DESC").should == result
     end
     
     it "should parse ORDER BY column_name     DESC" do
@@ -58,9 +66,24 @@ module CachedModel
       parse("ORDER BY col_one, col_two DESC").should_not be_nil
     end
     
+    it "should parse and eval ORDER BY col_one, col_two DESC" do
+      pair1 = Expression::OrderByPair.new(:col_one, Expression::OrderBy::ASC)
+      pair2 = Expression::OrderByPair.new(:col_two, Expression::OrderBy::DESC)
+      result = Expression::OrderBy.new(pair1, pair2)
+
+      parse_and_eval("ORDER BY col_one, col_two DESC").should == result
+    end
+    
     it "should parse ORDER BY col_one DESC, col_two" do
       parse("ORDER BY col_one DESC, col_one").should_not be_nil
     end
-
+    
+    it "should parse and eval ORDER BY col_one DESC, col_two" do
+      pair1 = Expression::OrderByPair.new(:col_one, Expression::OrderBy::DESC)
+      pair2 = Expression::OrderByPair.new(:col_two, Expression::OrderBy::ASC)
+      result = Expression::OrderBy.new(pair1, pair2)
+      
+      parse_and_eval("ORDER BY col_one DESC, col_two").should == result
+    end
   end
 end
