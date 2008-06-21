e23e5b482ff83bc665b69e5e1e778016bd4c807d
Scott Taylor <scott@railsnewbie.com>
2008-06-21 19:06:38 -0400

    * Adding rspec dependency

--- lib/guillotine/rake.rb
+++ lib/guillotine/rake.rb
@@ -2,5 +2,6 @@
 require 'rubygems'
 require 'rake'
 require 'spec/rake/spectask'
+require 'spec/rake/verify_rcov'
 require File.dirname(__FILE__) + "/rake/emacs"
 require File.dirname(__FILE__) + "/rake/treetop"
