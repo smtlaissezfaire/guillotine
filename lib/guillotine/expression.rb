192a8ee8bb0695af4d09c0705aa2761466521581
Scott Taylor <scott@railsnewbie.com>
2008-06-29 14:48:55 -0400

    * Using autoloading

--- lib/guillotine/expression.rb
+++ lib/guillotine/expression.rb
@@ -1,16 +1,5 @@
 expression = File.dirname(__FILE__) + "/expression"
 
-require "#{expression}/base"
-require "#{expression}/expressions"
-require "#{expression}/select"
-require "#{expression}/from"
-require "#{expression}/order_by"
-require "#{expression}/limit"
-require "#{expression}/backtick_string"
-require "#{expression}/truncate"
-require "#{expression}/delete"
-require "#{expression}/select_expression"
-
 module Guillotine
   module Expression
     class UnknownSyntaxError < StandardError; end
@@ -43,3 +32,23 @@ module Guillotine
     end
   end
 end
+
+
+Guillotine.autoload             :BackTickString,       "#{expression}/backtick_string"
+Guillotine::Expression.autoload :Base,                 "#{expression}/base"
+Guillotine::Expression.autoload :Equal,                "#{expression}/expressions"
+Guillotine::Expression.autoload :LessThan,             "#{expression}/expressions"
+Guillotine::Expression.autoload :GreaterThan,          "#{expression}/expressions"
+Guillotine::Expression.autoload :GreaterThanOrEqualTo, "#{expression}/expressions"
+Guillotine::Expression.autoload :LessThanOrEqualTo,    "#{expression}/expressions"
+Guillotine::Expression.autoload :NotEqual,             "#{expression}/expressions"
+Guillotine::Expression.autoload :IsNull,               "#{expression}/expressions"
+Guillotine::Expression.autoload :IsNotNull,            "#{expression}/expressions"
+Guillotine::Expression.autoload :Select,               "#{expression}/select"
+Guillotine::Expression.autoload :From,                 "#{expression}/from"
+Guillotine::Expression.autoload :OrderBy,              "#{expression}/order_by"
+Guillotine::Expression.autoload :Limit,                "#{expression}/limit"
+Guillotine::Expression.autoload :Truncate,             "#{expression}/truncate"
+Guillotine::Expression.autoload :TopLevelExpression,   "#{expression}/delete"
+Guillotine::Expression.autoload :Delete,               "#{expression}/delete"
+Guillotine::Expression.autoload :SelectExpression,     "#{expression}/select_expression"
