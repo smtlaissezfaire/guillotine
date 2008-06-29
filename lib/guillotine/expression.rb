a51d5094e29471498471288ad8730d1a3660c1ca
Scott Taylor <scott@railsnewbie.com>
2008-06-29 15:01:45 -0400

    * Cleaner autoloading

--- lib/guillotine/expression.rb
+++ lib/guillotine/expression.rb
@@ -1,5 +1,7 @@
 expression = File.dirname(__FILE__) + "/expression"
 
+Guillotine.autoload             :BackTickString,       "#{expression}/backtick_string"
+
 module Guillotine
   module Expression
     class UnknownSyntaxError < StandardError; end
@@ -30,25 +32,26 @@ module Guillotine
         }
       end
     end
+
+    
+    expression = File.dirname(__FILE__) + "/expression"
+    
+    autoload :Base,                 "#{expression}/base"
+    autoload :Equal,                "#{expression}/expressions"
+    autoload :LessThan,             "#{expression}/expressions"
+    autoload :GreaterThan,          "#{expression}/expressions"
+    autoload :GreaterThanOrEqualTo, "#{expression}/expressions"
+    autoload :LessThanOrEqualTo,    "#{expression}/expressions"
+    autoload :NotEqual,             "#{expression}/expressions"
+    autoload :IsNull,               "#{expression}/expressions"
+    autoload :IsNotNull,            "#{expression}/expressions"
+    autoload :Select,               "#{expression}/select"
+    autoload :From,                 "#{expression}/from"
+    autoload :OrderBy,              "#{expression}/order_by"
+    autoload :Limit,                "#{expression}/limit"
+    autoload :Truncate,             "#{expression}/truncate"
+    autoload :TopLevelExpression,   "#{expression}/delete"
+    autoload :Delete,               "#{expression}/delete"
+    autoload :SelectExpression,     "#{expression}/select_expression"
   end
 end
-
-
-Guillotine.autoload             :BackTickString,       "#{expression}/backtick_string"
-Guillotine::Expression.autoload :Base,                 "#{expression}/base"
-Guillotine::Expression.autoload :Equal,                "#{expression}/expressions"
-Guillotine::Expression.autoload :LessThan,             "#{expression}/expressions"
-Guillotine::Expression.autoload :GreaterThan,          "#{expression}/expressions"
-Guillotine::Expression.autoload :GreaterThanOrEqualTo, "#{expression}/expressions"
-Guillotine::Expression.autoload :LessThanOrEqualTo,    "#{expression}/expressions"
-Guillotine::Expression.autoload :NotEqual,             "#{expression}/expressions"
-Guillotine::Expression.autoload :IsNull,               "#{expression}/expressions"
-Guillotine::Expression.autoload :IsNotNull,            "#{expression}/expressions"
-Guillotine::Expression.autoload :Select,               "#{expression}/select"
-Guillotine::Expression.autoload :From,                 "#{expression}/from"
-Guillotine::Expression.autoload :OrderBy,              "#{expression}/order_by"
-Guillotine::Expression.autoload :Limit,                "#{expression}/limit"
-Guillotine::Expression.autoload :Truncate,             "#{expression}/truncate"
-Guillotine::Expression.autoload :TopLevelExpression,   "#{expression}/delete"
-Guillotine::Expression.autoload :Delete,               "#{expression}/delete"
-Guillotine::Expression.autoload :SelectExpression,     "#{expression}/select_expression"
