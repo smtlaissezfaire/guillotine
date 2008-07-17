c01471cd5477c1383265d0523da2311cef133505
Scott Taylor <scott@railsnewbie.com>
2008-07-17 04:21:52 -0400

    * Commenting out code, since there are no specs

--- lib/guillotine/extensions.rb
+++ lib/guillotine/extensions.rb
@@ -1,9 +1,10 @@
 
-begin
+# TODO: spec this load error
+# begin
   require "facets/indexable"
-rescue LoadError
-  raise LoadError, "You need to install the facets gem (sudo gem install facets)"
-end
+# rescue LoadError
+#   raise LoadError, "You need to install the facets gem (sudo gem install facets)"
+# end
 
 require File.dirname(__FILE__) + "/extensions/object"
 require File.dirname(__FILE__) + "/extensions/string"
