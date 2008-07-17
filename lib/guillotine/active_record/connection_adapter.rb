3d57cc3becd634f9f5bbda6c5890f81d1cb63b5e
Scott Taylor <scott@railsnewbie.com>
2008-07-17 02:55:34 -0400

    * Refactoring proxy

--- lib/guillotine/active_record/connection_adapter.rb
+++ lib/guillotine/active_record/connection_adapter.rb
@@ -2,28 +2,34 @@ module Guillotine
   module ActiveRecord
     class ConnectionAdapter
       module Proxy
-        def respond_to?(sym)
-          super || @connection.respond_to?(sym)
-        end
-        
-      private
-        
-        def method_missing(sym, *args, &blk)
-          if @connection.respond_to?(sym)
-            @connection.send(sym, *args, &blk)
-          else
-            super
+        def proxy!
+          class_eval do
+            def respond_to?(sym)
+              super || @connection.respond_to?(sym)
+            end
+            
+          private
+            
+            def method_missing(sym, *args, &blk)
+              if @connection.respond_to?(sym)
+                @connection.send(sym, *args, &blk)
+              else
+                super
+              end
+            end
+
           end
         end
       end
-      
-      include Proxy
       
       def initialize(active_record_connection)
         @connection = active_record_connection
       end
       
       attr_reader :connection
+      
+      extend Proxy
+      self.proxy!
     end
   end
 end
