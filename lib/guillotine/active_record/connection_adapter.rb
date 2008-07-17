4f3c0b7855192589d0dd52387fa268688a8aba4e
Scott Taylor <scott@railsnewbie.com>
2008-07-17 02:49:45 -0400

    * Refactor method_missing into a module

--- lib/guillotine/active_record/connection_adapter.rb
+++ lib/guillotine/active_record/connection_adapter.rb
@@ -1,23 +1,27 @@
 module Guillotine
   module ActiveRecord
     class ConnectionAdapter
+      module Proxy
+        def respond_to?(sym)
+          super || @connection.respond_to?(sym)
+        end
+        
+        def method_missing(sym, *args, &blk)
+          if @connection.respond_to?(sym)
+            @connection.send(sym, *args, &blk)
+          else
+            super
+          end
+        end
+      end
+      
+      include Proxy
+      
       def initialize(active_record_connection)
         @connection = active_record_connection
       end
       
       attr_reader :connection
-      
-      def respond_to?(sym)
-        super || @connection.respond_to?(sym)
-      end
-      
-      def method_missing(sym, *args, &blk)
-        if @connection.respond_to?(sym)
-          @connection.send(sym, *args, &blk)
-        else
-          super
-        end
-      end
     end
   end
 end
