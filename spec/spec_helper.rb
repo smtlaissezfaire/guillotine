187f3ebd1a31d99dc09730cf71bfee78c376793b
Scott Taylor <scott@railsnewbie.com>
2008-07-17 00:22:46 -0400

    * Adding spec support for activerecord

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,28 @@ module ParserSpecHelper
     parse(string).eval(*eval_args)
   end
 end
+
+module ActiveRecordSpecHelper
+  def setup_database_connection
+    database_yml = File.dirname(__FILE__) + "/database.yaml"
+    
+    ActiveRecord::Base.establish_connection YAML.load(File.open(database_yml))
+    ActiveRecord::Migration.verbose = false
+
+    ActiveRecord::Schema.define do  
+      create_table :users, :force => true do |t|
+        t.column :first_name, :string
+        t.column :last_name, :string
+        t.column :created_at, :datetime
+        t.column :updated_at, :datetime
+      end
+    end
+  end
+end
+
+include ActiveRecordSpecHelper
+setup_database_connection
+
+class User < ActiveRecord::Base
+  guillotine_model
+end
