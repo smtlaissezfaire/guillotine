a047776ba5b9e6a384724eeaf13c1c98d7640724
Scott Taylor <scott@railsnewbie.com>
2008-07-17 03:02:34 -0400

    * Use include instead of extend

--- lib/guillotine/active_record/connection_adapter.rb
+++ lib/guillotine/active_record/connection_adapter.rb
@@ -2,23 +2,29 @@ module Guillotine
   module ActiveRecord
     class ConnectionAdapter
       module Proxy
-        def proxy(ivar)
-          class_eval do
-            def respond_to?(sym)
-              super || __proxy_ivar__.respond_to?(sym)
-            end
-            
-          private
-            
-            define_method :__proxy_ivar__ do
-              instance_variable_get("@#{ivar}")
-            end
-            
-            def method_missing(sym, *args, &blk)
-              if __proxy_ivar__.respond_to?(sym)
-                __proxy_ivar__.send(sym, *args, &blk)
-              else
-                super
+        def self.included(other_mod)
+          other_mod.extend(ClassMethods)
+        end
+        
+        module ClassMethods
+          def proxy(ivar)
+            class_eval do
+              def respond_to?(sym)
+                super || __proxy_ivar__.respond_to?(sym)
+              end
+              
+            private
+              
+              define_method :__proxy_ivar__ do
+                instance_variable_get("@#{ivar}")
+              end
+              
+              def method_missing(sym, *args, &blk)
+                if __proxy_ivar__.respond_to?(sym)
+                  __proxy_ivar__.send(sym, *args, &blk)
+                else
+                  super
+                end
               end
             end
           end
@@ -31,7 +37,7 @@ module Guillotine
       
       attr_reader :connection
       
-      extend Proxy
+      include Proxy
       proxy :connection
     end
   end
