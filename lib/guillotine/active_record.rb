c48660eead1199f3f4d1ac19d6b8385183b1f90b
Scott Taylor <scott@railsnewbie.com>
2008-07-19 15:59:47 -0400

    * Don't autoload (we are requiring the file)

--- lib/guillotine/active_record.rb
+++ lib/guillotine/active_record.rb
@@ -3,7 +3,6 @@ module Guillotine
     dir = File.dirname(__FILE__) + "/active_record"
     
     require                            "#{dir}/extensions"
-    autoload :TableAdministrator,      "#{dir}/extensions"
     autoload :ConnectionAdapter,       "#{dir}/connection_adapter"
   end
 end
