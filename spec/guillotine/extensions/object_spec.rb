92014bbf1318a8618a0c1fea7518616eac00476d
Scott Taylor <scott@railsnewbie.com>
2008-07-19 17:07:44 -0400

    * Better scoping for specs

--- spec/guillotine/extensions/object_spec.rb
+++ spec/guillotine/extensions/object_spec.rb
@@ -1,25 +1,28 @@
 require File.dirname(__FILE__) + "/../../spec_helper"
 
 describe Object do
-  before :each do
-    @object = Object.new
-    @string = "foo bar baz"
-    @unmarshable_object = Class.new { }
-  end
   
-  it "should have the method object_size" do
-    @object.should respond_to(:object_size)
-  end
-  
-  it "should return the size of the marshaled string" do
-    @object.object_size.should == Marshal.dump(@object).size
-  end
-  
-  it "should return the size of a different object" do
-    @string.object_size.should == Marshal.dump(@string).size
-  end
-  
-  it "should return nil if the size cannot be calculated with Marshal" do
-    @unmarshable_object.object_size.should be_nil
+  describe "size" do
+    before :each do
+      @object = Object.new
+      @string = "foo bar baz"
+      @unmarshable_object = Class.new { }
+    end
+    
+    it "should have the method object_size" do
+      @object.should respond_to(:object_size)
+    end
+    
+    it "should return the size of the marshaled string" do
+      @object.object_size.should == Marshal.dump(@object).size
+    end
+    
+    it "should return the size of a different object" do
+      @string.object_size.should == Marshal.dump(@string).size
+    end
+    
+    it "should return nil if the size cannot be calculated with Marshal" do
+      @unmarshable_object.object_size.should be_nil
+    end
   end
 end
