ddb298bdb157ada4b970ab88a2cf5199c4cbf63a
Scott Taylor <scott@railsnewbie.com>
2008-07-19 20:03:27 -0400

    * Add pending specs for timed cache

---
+++ spec/guillotine/timed_cache_spec.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+module Guillotine
+  describe TimedCache do
+    describe "cache" do
+      it "should swap out the mysql connection adapter select"
+      
+      it "should call the block"
+      
+      it "should swap back the mysql connection adapter select"
+    end
+    
+    describe "swaping out the mysql connection"
+    
+    describe "swapping back the mysql connection adapter select method"
+  end
+end
