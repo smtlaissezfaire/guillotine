b474fd074df17acd93b0031e87cde73783756294
Scott Taylor <scott@railsnewbie.com>
2008-07-27 23:29:26 -0400

    * Refactoring

--- lib/guillotine/active_record/row_selector.rb
+++ lib/guillotine/active_record/row_selector.rb
@@ -10,9 +10,31 @@ module Guillotine
         if table = Guillotine::DataStore.table(table_name)
           parsed_sql.call(table)
         else
-          Guillotine::DataStore.create_table(table_name)
-          Guillotine::DataStore.initial_insert(table_name, ::ActiveRecord::Base.connection.__old_select_aliased_by_guillotine__("SELECT * FROM #{table_name}"))
+          create_table(table_name)
+          insert(table_name)
         end
+      end
+      
+    private
+      
+      def insert(table_name)
+        datastore.initial_insert(table_name, original_select("SELECT * FROM #{table_name}"))
+      end
+      
+      def create_table(table_name)
+        datastore.create_table(table_name)
+      end
+      
+      def original_select(query)
+        connection.__old_select_aliased_by_guillotine__(query)
+      end
+      
+      def connection
+        ::ActiveRecord::Base.connection
+      end
+      
+      def datastore
+        Guillotine::DataStore
       end
     end
   end
