d04a6a67f8eb647725e99cf1c609808eccc3253d
Scott Taylor <scott@railsnewbie.com>
2008-09-10 02:43:19 -0400

    * Add GroupBy#call

--- lib/guillotine/expressions/group_by.rb
+++ lib/guillotine/expressions/group_by.rb
@@ -14,6 +14,32 @@ module Guillotine
       def ==(other)
         @columns == other.columns
       end
+      
+      def call(a_collection)
+        return a_collection if a_collection.empty?
+        seen_values = []
+        
+        if columns.size > 1
+          GroupBy.new(first_column).call(a_collection) &
+          GroupBy.new(*columns[1..columns.size-1]).call(a_collection)
+        else
+          a_collection.select do |row|
+            value = row[first_column_name]
+            seen_values << value
+            seen_values.select{ |obj| obj == value }.size == 1
+          end
+        end
+      end
+      
+    private
+      
+      def first_column
+        columns.first
+      end
+      
+      def first_column_name
+        first_column.name
+      end
     end
   end
 end
--- spec/guillotine/expressions/group_by_spec.rb
+++ spec/guillotine/expressions/group_by_spec.rb
@@ -35,6 +35,129 @@ module Guillotine
           two.should_not == one
         end
       end
+      
+      describe "grouping" do
+        before :each do
+          @column_one.stub!(:name).and_return :foo
+          @column_three = mock 'column three'
+        end
+        
+        describe "grouping an empty collection" do
+          it "should return the collection" do
+            GroupBy.new(@column_one).call([]).should == []
+          end
+          
+          it "should return the collection if it's empty (using any sort of collection - duck type it)" do
+            a_collection = mock 'a collection'
+            a_collection.stub!(:empty?).and_return true
+            GroupBy.new(@column_one).call(a_collection).should == a_collection
+          end
+        end
+        
+        describe "grouping on a collection, with only one item" do
+          it "should return the record" do
+            @column_one.stub!(:name).and_return :foo
+            GroupBy.new(@column_one).call([{ :foo => :bar }]).should == [{ :foo => :bar }]
+          end
+        end
+        
+        describe "grouping on a collection on one row" do
+          it "should return the records when the data is not unique" do
+            records = [
+             { :foo => :bar },
+             { :foo => :quxx }
+            ]
+            @column_one.stub!(:name).and_return :foo
+            GroupBy.new(@column_one).call(records).should == records
+          end
+          
+          it "should return a record that column it's grouping on" do
+            records = [
+             { :foo => :bar },
+             { :foo => :bar }
+            ]
+            @column_one.stub!(:name).and_return(:foo)
+            GroupBy.new(@column_one).call(records).should == [{ :foo => :bar }]
+          end
+          
+          it "should return the first record it finds" do
+            records = [
+             { :foo => :bar, :baz => :quxx },
+             { :foo => :bar, :baz => :fnord }
+            ]
+            @column_one.stub!(:name).and_return(:foo)
+            GroupBy.new(@column_one).call(records).should == [{ :foo => :bar, :baz => :quxx }]
+          end
+          
+          it "should only group rows which are the same" do
+            records = [
+             { :foo => :bar },
+             { :foo => :bar },
+             { :foo => :quxx }
+            ]
+            @column_one.stub!(:name).and_return(:foo)
+            GroupBy.new(@column_one).call(records).should == [{ :foo => :bar }, { :foo => :quxx }]
+          end
+          
+          it "should work for a different row" do
+            records = [
+             { :bar => :bar },
+             { :bar => :bar },
+             { :bar => :quxx }
+            ]
+            @column_one.stub!(:name).and_return(:bar)
+            GroupBy.new(@column_one).call(records).should == [{ :bar => :bar }, { :bar => :quxx }]
+          end
+        end
+        
+        describe "grouping by two rows" do
+          it "should group by one row, if only one row has the same values" do
+            records = [
+             { :foo => :bar,  :bar => :bar },
+             { :foo => :bar,  :bar => :quxx }
+            ]
+            @column_one.stub!(:name).and_return(:foo)
+            @column_two.stub!(:name).and_return(:bar)
+            group = GroupBy.new(@column_one, @column_two)
+            group.call(records).should == [{ :foo => :bar, :bar => :bar}]
+          end
+          
+          it "should group by one row, if only one row has the same values, and it's the second row" do
+            records = [
+             { :foo => :baz,  :bar => :quxx },
+             { :foo => :bar,  :bar => :quxx }
+            ]
+            @column_one.stub!(:name).and_return(:foo)
+            @column_two.stub!(:name).and_return(:bar)
+            group = GroupBy.new(@column_one, @column_two)
+            group.call(records).should == [{ :foo => :baz, :bar => :quxx}]
+          end
+          
+          it "should return both rows, if neither are unique" do
+            records = [
+             { :foo => :baz,  :bar => :fnord },
+             { :foo => :bar,  :bar => :quxx }
+            ]
+            @column_one.stub!(:name).and_return(:foo)
+            @column_two.stub!(:name).and_return(:bar)
+            group = GroupBy.new(@column_one, @column_two)
+            group.call(records).should == records
+          end
+          
+          it "should group by 3 columns" do
+            records = [
+             first_record =  { :foo => :baz,   :bar => :fnord , :quxx => :fnord  },
+             second_record = { :foo => :bar,   :bar => :quxx,   :quxx => :fnord },
+             third_record =  { :foo => :fnord, :bar => :baz,    :quxx => :baz   }
+            ]
+            @column_one.stub!(:name).and_return(:foo)
+            @column_two.stub!(:name).and_return(:bar)
+            @column_three.stub!(:name).and_return(:quxx)
+            group = GroupBy.new(@column_one, @column_two, @column_three)
+            group.call(records).should == [first_record, third_record]
+          end
+        end
+      end
     end
   end
 end
