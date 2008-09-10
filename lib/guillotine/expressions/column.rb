1bd435641415090852cae63eb76066b8f93d9892
Scott Taylor <scott@railsnewbie.com>
2008-09-10 03:55:47 -0400

    * Use implied table names in the Column class

--- lib/guillotine/expressions/column.rb
+++ lib/guillotine/expressions/column.rb
@@ -1,16 +1,25 @@
 module Guillotine
   module Expressions
     class Column
-      def initialize(table_name, column_name)
-        @table_name = table_name
-        @column_name = column_name
+      def initialize(column)
+        column_as_string = column.to_s
+        
+        if column_as_string.include?(".")
+          @table_name, @column_name = column_as_string.split(".").map { |col| col.to_sym }
+        else
+          @column_name = column.to_sym
+        end
       end
       
-      attr_reader :table_name
+      attr_accessor :table_name
       attr_reader :column_name
       
       def to_sql
-        "#{table_name}.#{column_name}"
+        if table_name
+          "#{table_name}.#{column_name}"
+        else
+          column_name.to_s
+        end
       end
     end
   end
--- spec/guillotine/expressions/column_spec.rb
+++ spec/guillotine/expressions/column_spec.rb
@@ -3,26 +3,60 @@ require File.dirname(__FILE__) + "/../../spec_helper"
 module Guillotine
   module Expressions
     describe Column do
-      
-      it "should have a table name" do
-        Column.new(:foo, :bar).table_name.should equal(:foo)
+      it "should have a column name" do
+        Column.new(:bar).column_name.should equal(:bar)
       end
       
-      it "should have a column name" do
-        Column.new(:foo, :bar).column_name.should equal(:bar)
+      it "should have a nil table name if the table name is inflected" do
+        Column.new(:bar).table_name.should be_nil
+      end
+      
+      it "should be able to set the table name" do
+        column = Column.new(:bar)
+        column.table_name = :baz
+        column.table_name.should equal(:baz)
+      end
+      
+      it "should parse the table name when given a combined table / column name" do
+        column = Column.new("foo.bar")
+        column.table_name.should equal(:foo)
+      end
+      
+      it "should parse the *correct* table_name when given a combined table / column name" do
+        column = Column.new("baz.bar")
+        column.table_name.should equal(:baz)
+      end
+       
+      it "should parse the column name when given a combined table / column name" do
+        column = Column.new("foo.bar")
+        column.column_name.should equal(:bar)
+      end
+      
+      it "should find the table name even if the column_name is an interned string" do
+        column = Column.new("foo.bar".intern)
+        column.table_name.should equal(:foo)
+      end
+      
+      it "should always use a symbol for the column name" do
+        column = Column.new("foo")
+        column.column_name.should equal(:foo)
       end
       
       describe "to_sql" do
         it "should use the table and column name" do
-          Column.new(:foo, :bar).to_sql.should == "foo.bar"
+          Column.new("foo.bar").to_sql.should == "foo.bar"
         end
         
         it "should use the proper table name" do
-          Column.new(:bar, :bar).to_sql.should == "bar.bar"
+          Column.new("bar.bar").to_sql.should == "bar.bar"
         end
         
         it "should use the proper column name" do
-          Column.new(:bar, :fnord).to_sql.should == "bar.fnord"
+          Column.new("bar.fnord").to_sql.should == "bar.fnord"
+        end
+        
+        it "should use an ambiguous column name if no table is present" do
+          Column.new(:foo).to_sql.should == "foo"
         end
       end
     end
