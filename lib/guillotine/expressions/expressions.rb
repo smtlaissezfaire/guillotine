040252e9b426e172a8926d814f1fa1b3621ad4d4
Scott Taylor <scott@railsnewbie.com>
2008-09-10 20:57:01 -0400

    * Remove dynamic generation of sql classes

--- lib/guillotine/expressions/expressions.rb
+++ lib/guillotine/expressions/expressions.rb
@@ -1,27 +1,35 @@
 module Guillotine
   module Expressions
-    def self.classes_with_comparisons
-      @classes_with_comparisons ||= { 
-        :Equal                => :==,
-        :LessThan             => :<,
-        :GreaterThan          => :>,
-        :GreaterThanOrEqualTo => :>=,
-        :LessThanOrEqualTo    => :<=
-      }
-    end
-    
-    classes_with_comparisons.each do |class_name, comparison|
-      unless const_defined?(class_name)
-        klass = Class.new(Base) do
-          define_method :to_lambda do
-            new_lambda_with_comparison(comparison)
-          end
-        end
-        
-        const_set(class_name, klass) 
+    class Equal < Base
+      def to_lambda
+        new_lambda_with_comparison(:==)
       end
     end
-
+    
+    class LessThan < Base
+      def to_lambda
+        new_lambda_with_comparison(:<)
+      end
+    end
+    
+    class GreaterThan < Base
+      def to_lambda
+        new_lambda_with_comparison(:>)
+      end
+    end
+    
+    class GreaterThanOrEqualTo < Base
+      def to_lambda
+        new_lambda_with_comparison(:>=)
+      end
+    end
+    
+    class LessThanOrEqualTo < Base
+      def to_lambda
+        new_lambda_with_comparison(:<=)
+      end
+    end
+    
     class NotEqual < Equal
       def to_lambda
         lambda { |obj| ! super.call(obj) }
