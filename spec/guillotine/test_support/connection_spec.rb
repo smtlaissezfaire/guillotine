9409b64c9f52e849d466133b30f09e983f24a606
Scott Taylor <scott@railsnewbie.com>
2008-09-17 04:07:45 -0400

    * Refactor specs

--- spec/guillotine/test_support/connection_spec.rb
+++ spec/guillotine/test_support/connection_spec.rb
@@ -6,10 +6,6 @@ module Guillotine
       before :each do
         @datastore = mock 'connection'
         @connection = Connection.new(@datastore)
-        @select_class = mock 'a select class'
-        Guillotine.stub!(:execute).and_return @select_class
-        @from = mock 'a from clause', :table_name => :foo
-        @select_class.stub!(:from).and_return @from
       end
       
       it "should truncate all the tables in the datastore on rollback" do
@@ -26,12 +22,19 @@ module Guillotine
       end
       
       describe "select" do
+        before :each do
+          @select = mock 'a select'
+          Guillotine.stub!(:execute).and_return @select
+          @from = mock 'a from clause', :table_name => :foo
+          @select.stub!(:from).and_return @from
+        end
+        
         it "should have a select method" do
           @connection.should respond_to(:select)
         end
         
         it "should call Guillotine.execute with the select query given" do
-          Guillotine.should_receive(:execute).with("SELECT * FROM users").and_return @select_class
+          Guillotine.should_receive(:execute).with("SELECT * FROM users").and_return @select
           @connection.select("SELECT * FROM users")
         end
         
