8a69c5e8a84ee13023c3c31c350ec6c7f188ae7a
Scott Taylor <scott@railsnewbie.com>
2008-09-17 23:16:07 -0400

    * Add basic parsing INSERT

--- lib/guillotine/parser.rb
+++ lib/guillotine/parser.rb
@@ -38,6 +38,7 @@ module Guillotine
   require "#{parser}/sql_delete"
   require "#{parser}/sql_drop_table"
   require "#{parser}/sql_show_tables"
-  require "#{parser}/sql_transaction"  
+  require "#{parser}/sql_transaction"
+  require "#{parser}/sql_insert"  
   require "#{parser}/sql"
 end
