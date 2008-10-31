e652745e66aa24053580e0c823bceb739ed91813
Scott Taylor <scott@railsnewbie.com>
2008-10-30 22:41:20 -0400

    * move header definitions into .h

--- ext/quotes_parser/quotes_parser.c
+++ ext/quotes_parser/quotes_parser.c
@@ -2,32 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include "ruby.h"
-
-static char *       original_string = NULL;
-static char *       buffer          = NULL;
-static unsigned int at              = 0;
-static bool         in_quotes       = false;
-static char         starting_quote;
-
-static char * parse();
-static bool   chars_left();
-static char   current_char();
-static char   next_char();
-static void   eat_whitespace();
-static void   add_current_char();
-static void   add_to_buffer(char);
-static void   update_quote_status();
-static int    advance_char();
-static bool   a_quote(char);
-
-#define SINGLE_QUOTE '\''
-#define DOUBLE_QUOTE '\"'
-#define BACKTICK     '`'
-#define SPACE        ' '
-
-VALUE QuotesParser = Qnil;
-void  Init_quotes_parser();
-VALUE quotes_parser(VALUE, VALUE);
+#include "quotes_parser.h"
 
 static char * parse(char * string) {
   at = 0;
