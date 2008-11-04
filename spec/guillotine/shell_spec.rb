44b5095100f837ebf4051be29bcc66aa2437a307
Scott Taylor <scott@railsnewbie.com>
2008-11-04 03:26:14 -0500

    * use better spec names

--- spec/guillotine/shell_spec.rb
+++ spec/guillotine/shell_spec.rb
@@ -1,14 +1,14 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
 module Guillotine
-  module Shell
+  describe Shell do
     describe "start" do
       before(:each) do
-        Main.stub!(:do)
+        Shell::Main.stub!(:do)
       end
       
       it "should call Main.do" do
-        Main.should_receive(:do)
+        Shell::Main.should_receive(:do)
         Shell.start
       end
     end
