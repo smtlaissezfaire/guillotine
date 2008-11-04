429a4f18745764d6079957758c351882b3bd890e
Scott Taylor <scott@railsnewbie.com>
2008-11-04 03:30:46 -0500

    * Move main into it's own file

--- lib/guillotine/shell.rb
+++ lib/guillotine/shell.rb
@@ -5,44 +5,8 @@ module Guillotine
     end
     
     dir = "#{File.dirname(__FILE__)}/shell"
+    autoload :Main, "#{dir}/main"
     autoload :Command, "#{dir}/command"
     autoload :OutputFormatter, "#{dir}/output_formatter"
-    
-    module Main
-      INTRODUCTORY_TEXT = File.read(File.dirname(__FILE__) + "/shell_introduction.txt")
-      
-      class << self
-        def do
-          puts(INTRODUCTORY_TEXT)
-          
-          loop do
-            begin
-              printf(">> ")
-              Command.execute(gets)
-            rescue => e
-              output_error(e)
-            end
-          end
-        end
-        
-        def output_error(e)
-          puts e.message
-        end
-        
-      private
-        
-        def puts(*args)
-          Kernel.puts(*args)
-        end
-        
-        def printf(*args)
-          Kernel.printf(*args)
-        end
-        
-        def gets(*args)
-          Kernel.gets(*args).chomp
-        end
-      end
-    end
   end
 end
---
+++ lib/guillotine/shell/main.rb
@@ -0,0 +1,41 @@
+module Guillotine
+  module Shell
+    module Main
+      INTRODUCTORY_TEXT = File.read(File.dirname(__FILE__) + "/../shell_introduction.txt")
+      
+      class << self
+        def do
+          puts(INTRODUCTORY_TEXT)
+          
+          loop do
+            begin
+              printf(">> ")
+              Command.execute(gets)
+            rescue => e
+              output_error(e)
+            end
+          end
+        end
+        
+        def output_error(e)
+          puts e.message
+        end
+        
+      private
+        
+        def puts(*args)
+          Kernel.puts(*args)
+        end
+        
+        def printf(*args)
+          Kernel.printf(*args)
+        end
+        
+        def gets(*args)
+          Kernel.gets(*args).chomp
+        end
+      end
+    end
+  end
+end
+
