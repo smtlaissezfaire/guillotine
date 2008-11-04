63cf4d2eba3e52048fffd8b3c5e99a68a96e8e0b
Scott Taylor <scott@railsnewbie.com>
2008-11-04 03:32:34 -0500

    * Re-arrange and align text

--- lib/guillotine/shell.rb
+++ lib/guillotine/shell.rb
@@ -1,12 +1,13 @@
 module Guillotine
   module Shell
+    dir = "#{File.dirname(__FILE__)}/shell"
+    
+    autoload :Main,            "#{dir}/main"
+    autoload :Command,         "#{dir}/command"
+    autoload :OutputFormatter, "#{dir}/output_formatter"
+    
     def self.start
       Main.do
     end
-    
-    dir = "#{File.dirname(__FILE__)}/shell"
-    autoload :Main, "#{dir}/main"
-    autoload :Command, "#{dir}/command"
-    autoload :OutputFormatter, "#{dir}/output_formatter"
   end
 end
