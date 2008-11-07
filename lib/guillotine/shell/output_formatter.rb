ee71e16dfa2190ea13529a5f36912c57c701eec0
Scott Taylor <scott@railsnewbie.com>
2008-11-06 22:58:05 -0500

    * Add format method

--- lib/guillotine/shell/output_formatter.rb
+++ lib/guillotine/shell/output_formatter.rb
@@ -5,8 +5,47 @@ module Guillotine
         new.format(obj)
       end
       
-      def format(obj)
-        obj.to_s
+      def format(table)
+        out = ""
+        out << header(table)
+        records = table
+        records.each do |record|
+          out << output_record(record, records)
+        end
+        out
+      end
+      
+    private
+      
+      def spaces(records, column_name, column_value)
+        length = max_value_length(records, column_name)
+        " " * (length - column_value.to_s.length)
+      end
+      
+      def max_value_length(records, column_name)
+        records.map { |record| record[column_name].to_s.length }.max
+      end
+
+      # TODO: Refactor later, when less tired
+      def header(table)
+        str = "| "
+        str << column_names(table).map do |column_name|
+          "#{column_name}#{spaces(table, column_name, column_name)}"
+        end.join(" | ")
+        
+        "#{str} |\n"
+      end
+      
+      def output_record(record, records)
+        out = "| "
+        out << record.map do |column_name, value|
+          "#{value}#{spaces(records, column_name, value)}"
+        end.join(" | ")
+        out << " |\n"
+      end
+      
+      def column_names(rows)
+        rows.first.keys
       end
     end
   end
--- spec/guillotine/shell/output_formatter_spec.rb
+++ spec/guillotine/shell/output_formatter_spec.rb
@@ -8,11 +8,6 @@ module Guillotine
         @obj = mock 'an-object'
       end
       
-      it "should call to_s on the object" do
-        @obj.should_receive(:to_s).and_return "some result"
-        @formatter.format(@obj)
-      end
-      
       describe "class format convenience method" do
         before(:each) do
           @formatter = mock 'formatter', :format => "some output"
@@ -29,6 +24,52 @@ module Guillotine
         it "should call format with the object" do
           @formatter.should_receive(:format).with(@an_object).and_return "some output"
           Shell::OutputFormatter.format(@an_object)
+        end
+      end
+      
+      describe "format" do
+        it "should have one column name" do
+          @formatter.format([{ :foo => :bar}]).should include("foo")
+        end
+        
+        it "should output two column names" do
+          @formatter.format([{ :foo => 123, :bar => :baz }]).should include("foo | bar")
+        end
+        
+        it "should have a vertical bar before and after the column name" do
+          @formatter.format([{ :foo => :bar }]).should include("| foo |")
+        end
+        
+        it "should have a vertical bar between both columns" do
+          @formatter.format([{ :foo => 123, :bar => :baz }]).should include("| foo | bar |")          
+        end
+        
+        it "should have the first value in the table" do
+          @formatter.format([{ :foo => 123 }]).should include("123")
+        end
+        
+        it "should include the correct value" do
+          @formatter.format([{ :foo => 3234 }]).should include("3234")
+        end
+        
+        it "should have 5 spaces for a 3 value column" do
+          @formatter.format([{ :foo => "123" }]).should include("| 123 |")
+        end
+        
+        it "should include the a 3 value column with 6 spaces when a different value takes 4 spaces" do
+          @formatter.format([{ :foo => "123" }, { :foo => "1234" }]).should include("| 123  |")
+        end
+        
+        it "should include multiple columns spaced out appropriately" do
+          @formatter.format([{ :foo => "123" }, { :foo => "123456" }]).should include("| 123    |")
+        end
+        
+        it "should space out the columns appropriately" do
+          @formatter.format([{ :foo => "123" }, { :foo => "123456" }]).should include("| foo    |")
+        end
+        
+        it "should map two values side by side" do
+          @formatter.format([{ :foo => "123", :bar => "123" }, { :foo => "123456", :bar => "123" }]).should include("| 123    | 123 |")
         end
       end
     end
