6eae701ed794c0be7e8a73df995f49ca4cecc8c2
Scott Taylor <scott@railsnewbie.com>
2008-11-09 13:28:03 -0500

    * Add datatypes to CREATE TABLE

--- spec/guillotine/parser/sql_create_table_spec.rb
+++ spec/guillotine/parser/sql_create_table_spec.rb
@@ -10,19 +10,39 @@ module Guillotine
       end
       
       it "should parse a simple create table with one column" do
-        parse("CREATE TABLE foo (foo bit)").should_not be_nil
+        parse("CREATE TABLE foo (foo BIT)").should_not be_nil
       end
       
       it "should parse it with any number of spaces" do
-        parse("CREATE   TABLE   foo (foo bit)").should_not be_nil
+        parse("CREATE   TABLE   foo (foo BIT)").should_not be_nil
       end
       
-      it "should allow an arbitrary table name" do
-        parse("CREATE   TABLE   bar (foo bit)").should_not be_nil
+      it "should allow an arBITrary table name" do
+        parse("CREATE   TABLE   bar (foo BIT)").should_not be_nil
       end
       
       it "should allow backquoting of the tablename" do
-        parse("CREATE   TABLE   `bar` (foo bit)").should_not be_nil
+        parse("CREATE   TABLE   `bar` (foo BIT)").should_not be_nil
+      end
+      
+      it "should allow a different column name" do
+        parse("CREATE TABLE foo (bar BIT)").should_not be_nil
+      end
+      
+      it "should allow backticking of the column name" do
+        parse("CREATE TABLE foo (`bar` BIT)").should_not be_nil
+      end
+      
+      it "should allow spaces between the open parens" do
+        parse("CREATE TABLE foo   (   foo BIT)").should_not be_nil
+      end
+      
+      it "should allow space after the parens" do
+        parse("CREATE TABLE foo (foo BIT  )").should_not be_nil
+      end
+      
+      it "should allow an int" do
+        parse("CREATE TABLE foo ( foo INT (11))").should_not be_nil
       end
     end
   end
