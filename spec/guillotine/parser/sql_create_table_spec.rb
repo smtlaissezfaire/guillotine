f8bcc48224c264ac557bb4aea276c42e4e1c5302
Scott Taylor <scott@railsnewbie.com>
2008-11-09 15:24:13 -0500

    * Add eval to create table

--- spec/guillotine/parser/sql_create_table_spec.rb
+++ spec/guillotine/parser/sql_create_table_spec.rb
@@ -7,10 +7,40 @@ module Guillotine
       
       before :each do
         @parser = SQLCreateTableParser.new
+        @column = mock 'column'
+        @create_table = mock 'create table'
+        Expressions::CreateTable.stub!(:new).and_return @create_table
       end
       
       it "should parse a simple create table with one column" do
         parse("CREATE TABLE foo (foo BIT)").should_not be_nil
+      end
+      
+      it "should instantiate a create table" do
+        Expressions::CreateTable.should_receive(:new).and_return @create_table
+        parse_and_eval("CREATE TABLE foo (foo BIT)")
+      end
+      
+      it "should instantiate with a table name" do
+        Expressions::CreateTable.should_receive(:new).with(hash_including(:table_name => "foo")).and_return @create_table
+        parse_and_eval("CREATE TABLE foo (foo BIT)")
+      end
+      
+      it "should instantiate with the correct table name" do
+        Expressions::CreateTable.should_receive(:new).with(hash_including(:table_name => "bar")).and_return @create_table
+        parse_and_eval("CREATE TABLE bar (foo BIT)")
+      end
+      
+      it "should receive one column" do
+        Expressions::Column.stub!(:new).and_return @column
+        Expressions::CreateTable.should_receive(:new).with(hash_including(:columns => [@column])).and_return @create_table
+        parse_and_eval("CREATE TABLE bar (foo BIT)")
+      end
+      
+      it "should instantiate with two columns in an array" do
+        Expressions::Column.stub!(:new).and_return @column
+        Expressions::CreateTable.should_receive(:new).with(hash_including(:columns => [@column, @column])).and_return @create_table
+        parse_and_eval("CREATE TABLE bar (foo BIT, bar BIT)")
       end
       
       it "should parse it with any number of spaces" do
