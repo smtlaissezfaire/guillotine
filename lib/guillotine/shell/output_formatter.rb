393e56473f596d28016fd215e3ff7c8e97e70c30
Scott Taylor <scott@railsnewbie.com>
2008-11-16 20:50:00 -0500

    * Add optional end vertical bar.  Fix max_size to be the size of the string

--- lib/guillotine/shell/output_formatter.rb
+++ lib/guillotine/shell/output_formatter.rb
@@ -2,32 +2,41 @@ module Guillotine
   module Shell
     class OutputFormatter
       class ColumnOutputer
-        def self.output(string, max_size)
-          new(string, max_size).output
+        def self.output(string, max_size, end_column = false)
+          new(string, max_size, end_column).output
         end
         
         class InvalidMaxSizeError < StandardError; end
         
-        def initialize(string, max_size)
+        BAR_SEPARATOR = "|"
+        SPACE = " "
+        EMPTY_STRING = ""
+        
+        def initialize(string, max_size, end_column = false)
           @string = string
           @max_size = max_size
+          @end_column = end_column
         end
         
         def output
-          "| #{@string}#{spaces} |"
+          "#{BAR_SEPARATOR}#{SPACE}#{@string}#{spaces}#{SPACE}#{end_column_bar}"
         end
         
         def spaces
-          " " * number_of_spaces
+          SPACE * number_of_spaces
         end
         
         def number_of_spaces
-          space_count = @max_size - 4 - @string.length
+          space_count = @max_size - @string.length
           if space_count < 0
             raise(InvalidMaxSizeError,
                   "The string '#{@string}' cannot fit into a column #{@max_size} chars wide")
           end
           space_count
+        end
+        
+        def end_column_bar
+          @end_column ? BAR_SEPARATOR : EMPTY_STRING
         end
       end
       
--- spec/guillotine/shell/output_formatter/column_outputer_spec.rb
+++ spec/guillotine/shell/output_formatter/column_outputer_spec.rb
@@ -8,37 +8,41 @@ module Guillotine
       end
       
       it "should output a string with 'foo' with a max size of 7" do
-        @outputer.output("foo", 7).should == "| foo |"
+        @outputer.output("foo", 3).should == "| foo "
       end
       
       it "should output the correct string" do
-        @outputer.output("bar", 7).should == "| bar |"
+        @outputer.output("bar", 3).should == "| bar "
       end
       
       it "should add one extra space" do
-        @outputer.output("bar", 8).should == "| bar  |"
+        @outputer.output("bar", 4).should == "| bar  "
       end
       
       it "should add two extra spaces" do
-        @outputer.output("foo", 9).should == "| foo   |"
+        @outputer.output("foo", 5).should == "| foo   "
+      end
+      
+      it "should not add the end bar if its not an an end column" do
+        @outputer.output("foo", 3, true).should == "| foo |"
       end
       
       it "should raise an error if there is not enough space" do
         lambda {
-          @outputer.output("foo", 6)
-        }.should raise_error(OutputFormatter::ColumnOutputer::InvalidMaxSizeError, "The string 'foo' cannot fit into a column 6 chars wide")
+          @outputer.output("foo", 2)
+        }.should raise_error(OutputFormatter::ColumnOutputer::InvalidMaxSizeError, "The string 'foo' cannot fit into a column 2 chars wide")
       end
       
       it "should raise an error with the correct column name" do
         lambda {
-          @outputer.output("bar", 6)
-        }.should raise_error(OutputFormatter::ColumnOutputer::InvalidMaxSizeError, "The string 'bar' cannot fit into a column 6 chars wide")
+          @outputer.output("bar", 2)
+        }.should raise_error(OutputFormatter::ColumnOutputer::InvalidMaxSizeError, "The string 'bar' cannot fit into a column 2 chars wide")
       end
       
       it "should raise an error with the correct number of chars passed" do
         lambda {
-          @outputer.output("bar", 2)
-        }.should raise_error(OutputFormatter::ColumnOutputer::InvalidMaxSizeError, "The string 'bar' cannot fit into a column 2 chars wide")
+          @outputer.output("bar", 1)
+        }.should raise_error(OutputFormatter::ColumnOutputer::InvalidMaxSizeError, "The string 'bar' cannot fit into a column 1 chars wide")
       end
     end
   end
