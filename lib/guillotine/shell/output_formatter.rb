0fe62dbaf1d291df7fcbe22a147af4c427119cd3
Scott Taylor <scott@railsnewbie.com>
2008-11-17 01:04:31 -0500

    * Move ColumnLengthCalculator into it's own file

--- lib/guillotine/shell/output_formatter.rb
+++ lib/guillotine/shell/output_formatter.rb
@@ -6,30 +6,7 @@ module Guillotine
       autoload :ColumnOutputer, "#{dir}/column_outputer"
       autoload :ColumnDelimiterHeader, "#{dir}/column_delimiter_header"
       autoload :ColumnExtractor, "#{dir}/column_extractor"
-      
-      module ColumnLengthCalculator
-        class << self
-          def size_of_column(column, values)
-            column_length = column_length(column)
-            value_length = max_value_length(values)
-            
-            column_length > value_length ?
-              column_length :
-              value_length
-          end
-          
-        private
-          
-          def column_length(column)
-            column.to_s.length
-          end
-          
-          def max_value_length(values)
-            value_lengths = values.map { |value| value.length }
-            value_lengths.sort.last
-          end
-        end
-      end
+      autoload :ColumnLengthCalculator, "#{dir}/column_length_calculator"
       
       def self.format(obj)
         new.to_s(obj)
